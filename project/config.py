ab65ae95a0d92e05593422e4eede19aca6d64244
aravind2225 <aravindudiyana123@gmail.com>
2026-01-30 15:43:23 +0530

    updated and added

--- project/config.py
+++ project/config.py
@@ -1,11 +1,11 @@
 class Config:
     SECRET_KEY = "dev-secret-key"
 
-    DB_USER = "postgres"
-    DB_PASSWORD = "techv1%403"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-    DB_NAME = "log_file"
+    DB_USER = "postgres.bbyqpjdlmrvqdegpbygy"
+    DB_PASSWORD = "tjs@cW%$b&598V6"
+    DB_HOST = "aws-1-ap-south-1.pooler.supabase.com"
+    DB_PORT = "6543"
+    DB_NAME = "postgres"
 
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
